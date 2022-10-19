e535a6d4fdb761e994b8de94f782859e20d207a4
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-19 16:37:23 +0530

    Updated with all changes changed namespace changed classaname Added private methods

---
+++ EncryptDecrypt/CeaserCipher.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CaeserCipherAlgorithm
+{
+    public class CaeserCipher
+    {
+        private string EncryptedText = "";
+        private string DeccryptedText = "";
+        private bool IsAlphabet(int charCheck)
+        {
+            return ((IsCapitalAlphabet(charCheck) || IsSmallAlphabet(charCheck)));
+        }
+        private bool IsCapitalAlphabet(int charCheck)
+        {
+            return (charCheck >= 65 && charCheck <= 90);
+        }
+        private bool IsSmallAlphabet(int charCheck)
+        {
+            return (charCheck >= 97 && charCheck <= 122);
+        }
+        public string DoCeaserCipher(string inputString,int shiftingFactor) // Forward dir
+        {
+
+            if (shiftingFactor > 0) //if we want to do encryption forward
+            {
+                for (int count = 0; count < inputString.Length; count++)
+                {
+                    int charInt = (int)(inputString[count]);
+                    if (IsAlphabet(charInt))
+                    {
+                        int charIntWithFactor = charInt + shiftingFactor;
+                        if ((IsCapitalAlphabet(charIntWithFactor) && IsCapitalAlphabet(charInt)) || ((IsSmallAlphabet(charIntWithFactor) && IsSmallAlphabet(charInt))))
+                        {
+                            char c = (char)charIntWithFactor;
+                            EncryptedText += c;
+                        }
+                        else
+                        {
+                            if (IsCapitalAlphabet(charInt))
+                            {
+                                charIntWithFactor = 64 + (charIntWithFactor % 90);
+                                char c = (char)charIntWithFactor;
+                                EncryptedText += c;
+                            }
+                            else if (IsSmallAlphabet(charInt)) //lower
+                            {
+                                charIntWithFactor = 96 + (charIntWithFactor % 122);
+                                char c = (char)charIntWithFactor;
+                                EncryptedText += c;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        EncryptedText += inputString[count];
+                    }
+                }
+                return EncryptedText;
+            }
+
+            else
+            {
+                for (int count = 0; count < inputString.Length; count++)
+                {
+                    int charInt = (int)(inputString[count]);
+                    if (IsAlphabet(charInt))
+                    {
+                        int charIntwithFactor = charInt + shiftingFactor;
+                        if ((IsCapitalAlphabet(charIntwithFactor) && IsCapitalAlphabet(charInt)) || ((IsSmallAlphabet(charIntwithFactor) && IsSmallAlphabet(charInt))))
+                        {
+                            char c = (char)charIntwithFactor;
+                            DeccryptedText += c;
+                        }
+
+                        else
+                        {
+                            if (IsCapitalAlphabet(charInt))
+                            {
+                                if (charIntwithFactor < 65)
+                                {
+                                    charIntwithFactor = 90 - (64 - charIntwithFactor);
+                                }
+                                char c = (char)charIntwithFactor;
+                                DeccryptedText += c;
+                            }
+                            else if (IsSmallAlphabet(charInt) && charIntwithFactor < 97) //lower
+                            {
+                                charIntwithFactor = 122 - (96 - charIntwithFactor);
+                                char c = (char)charIntwithFactor;
+                                DeccryptedText += c;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        DeccryptedText += inputString[count];
+                    }
+                }
+                return DeccryptedText;
+            }
+            
+        }
+    }
+}
---
+++ EncryptDecrypt/Program.cs
@@ -0,0 +1,59 @@
+﻿using CaeserCipherAlgorithm;
+
+public class Program{
+    public static void Main()
+    {
+        try
+        {
+            int choice = 0;
+            Console.Write("Enter text:");
+            string inputString = Console.ReadLine();
+            Console.Write("Enter Shifting factor:");
+            int shiftingFactor = int.Parse(Console.ReadLine());
+
+            if (shiftingFactor < 0)
+            {
+                throw new Exception("factor must be greater than 0");
+            }
+            Console.WriteLine("Press:\n1.Encrypt\n2.Decrypt\n0.Exit\n");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                CaeserCipher obj=new CaeserCipher();
+                
+                string eText = obj.DoCeaserCipher(inputString, shiftingFactor);
+                Console.WriteLine("Encrypted Text:" + eText);
+            }
+            else if (choice == 2)
+            {
+                CaeserCipher obj = new CaeserCipher();
+                string dText = obj.DoCeaserCipher(inputString, -(shiftingFactor));
+                Console.WriteLine("Decrypted Text:" + dText);
+            }
+            else if (choice == 0)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                throw new Exception("Invalid Choice");
+                Console.WriteLine();
+
+            }
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+
+        }
+        catch (NullReferenceException ex1)
+        {
+            Console.WriteLine(ex1.Message);
+            Console.WriteLine();
+        }
+
+    }
+}
